9673ef41ba623b9b9026e2893ef11c28fb5f955e
Владимир <tovlivanov@yandex.ru>
2020-09-21 16:35:45 +0400

    4. Переопределение [#334049]

---
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+    public class JSONReport extends TextReport {
+        public String generate(String name, String body) {
+            return "{\n" +
+                    "  \"name\" : \"name\",\n" +
+                    "  \"body\" : \"body\"\n" +
+                    "}";
+        }
+        public static void main(String[] args) {
+            JSONReport report = new JSONReport();
+            String text = report.generate("Report's name", "Report's body");
+            System.out.println(text);
+        }
+}
