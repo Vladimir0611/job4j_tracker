1ae838f8ef73f150eec5f86ca67332b21ed7c7e3
Владимир <tovlivanov@yandex.ru>
2020-11-24 16:03:30 +0400

    3. Интерфейс Input [#333986]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void toGo() {
+
+    }
+
+    @Override
+    public void passengers(String passengers) {
+
+    }
+
+    @Override
+
+    public int refill(String fuel) {
+        return 10;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void toGo ();
+
+    void passengers (String question);
+
+    int refill (String question);
+}
