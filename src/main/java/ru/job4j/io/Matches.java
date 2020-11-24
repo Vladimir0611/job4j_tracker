d77b670e83fdc45a03b981c9c87bb09fd331cea4
Владимир <tovlivanov@yandex.ru>
2020-11-24 14:35:35 +0400

    2.1. Реализация класса StartUI [#333996]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -6,47 +6,33 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int quantity = 11;
-        int i1 = 1;
-        int i2 = 1;
+        int i = 1;
         while (quantity > 0) {
-            while (i1 > 0) {
+            while (i < 3) {
                 System.out.println("Количество спичек на столе:" + quantity);
-                System.out.println("Игрок 1 берет спички:");
+                System.out.println("Игрок " + i + " берет спички: ");
                 int select = Integer.valueOf(input.nextLine());
-                if (select <= 3 && select >= 1) {
+                if (select <= 3 && select >= 1 && select <= quantity) {
                     quantity = quantity - select;
-                    System.out.println("Количество спичек взятое Игроком 1:" + select);
-                    System.out.println("Количество спичек на столе:" + quantity);
-                } else {
-                    System.out.println("Необходимо взять от 1 до 3 спичек:");
-                    continue;
-                }
-                i1 = 0;
-            }
-            if (quantity == 0) {
-                System.out.println("Игрок 1 победил");
-                break;
-            }
-
-            while (i2 > 0) {
-                System.out.println("Игрок 2 берет спички:");
-                int select2 = Integer.valueOf(input.nextLine());
-                if (select2 <= 3 && select2 >= 1) {
-                    quantity = quantity - select2;
-                    System.out.println("Количество спичек взятое Игроком 1:" + select2);
+                    System.out.println("Количество спичек взятое Игроком " + i + " : " + select);
 
                 } else {
-                    System.out.println("Необходимо взять от 1 до 3 спичек:");
+                    if (select > quantity) {
+                        System.out.println("Вы хотите взять больше спичек чем есть на столе:");
+                    } else {
+                        System.out.println("Необходимо взять от 1 до 3 спичек:");
+                    }
                     continue;
                 }
-                i2 = 0;
+
+                    if (quantity == 0) {
+                    System.out.println("Игрок " + i + "  победил");
+                    break;
+                }
+                i++;
             }
-            if (quantity == 0) {
-                System.out.println("Игрок 2 победил");
-                break;
-            }
-            i1 = 1;
-            i2 = 1;
+            i = 1;
+
         }
     }
 }
