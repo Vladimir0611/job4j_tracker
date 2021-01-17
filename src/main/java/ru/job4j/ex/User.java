87a7ada44371bdcfe28d5909988f596eb3e646d1
Владимир <tovlivanov@yandex.ru>
2021-01-17 17:01:23 +0300

    0.5. Иерархия исключений и множественный catch. [#334007]

--- src/main/java/ru/job4j/ex/User.java
+++ src/main/java/ru/job4j/ex/User.java
@@ -4,10 +4,13 @@ public class User {
     private String username;
     private boolean valid;
 
+
+
     public User(String username, boolean valid) {
         this.username = username;
         this.valid = valid;
     }
+
 
     public String getUsername() {
         return username;
