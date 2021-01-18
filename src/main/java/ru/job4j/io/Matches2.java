8ecaf4a2dd635ea46821c2a43afeb3de13870676
Владимир <tovlivanov@yandex.ru>
2021-01-18 17:39:56 +0300

    2.

---
+++ src/main/java/ru/job4j/io/Matches2.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int quantity = 11;
+        int i = 1;
+        while (quantity > 0) {
+            while (i < 3) {
+                System.out.println("Количество спичек на столе:" + quantity);
+                System.out.println("Игрок " + i + " берет спички: ");
+                int select = Integer.valueOf(input.nextLine());
+                if (select <= 3 && select >= 1 && select <= quantity) {
+                    quantity = quantity - select;
+                    System.out.println("Количество спичек взятое Игроком " + i + " : " + select);
+
+                } else {
+                    if (select > quantity) {
+                        System.out.println("Вы хотите взять больше спичек чем есть на столе:");
+                    } else {
+                        System.out.println("Необходимо взять от 1 до 3 спичек:");
+                    }
+                    continue;
+                }
+
+                i++;
+            }
+            if (quantity == 0) {
+                System.out.println("Игрок " + i + "  победил");
+                break;
+            }
+            i = 1;
+        }
+    }
+}
